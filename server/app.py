51d597447ece97d4510879614b554b3251409d37
prokarius <rollingwithoutslipping@yahoo.com>
2018-01-27 19:20:59 +0800

    Added database table stuff

--- server/app.py
+++ server/app.py
@@ -1,20 +1,34 @@
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api, reqparse
+from flask_sqlalchemy import SQLAlchemy
 
 import ast
 
-from LZM import compress 
-from RLE import compress1
-from WDE import compressWDE
-from BusyTrain import busybody
-from sort import qsort
-from EmptyArea import rectangle, circle 
-from jewellery_heist import fakeknapsack
-from releaseSchedule import scheduler
-from horse import horsenipples
-
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_API'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
 api = Api(app)
+
+class Food(db.Model):
+    foodid = db.Column(db.Integer, primary_key=True)
+    cook = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(2000), nullable=False)
+    placeLong = db.Column(db.Float, nullable=False)
+    placeLat = db.Column(db.Float, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+# This is for people who are particiapting in the dining
+class FoodPeople(db.Model):
+    foodid = db.Column(db.Integer, ForeignKey('Food.foodid'), nullable=False)
+    people = db.Column(db.String(120), nullable=False)
+
+class FeedbackPeople(db.Model):
+    foodid = db.Column(db.Integer, ForeignKey('Food.foodid'), nullable=False)
+    people = db.Column(db.String(120), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
 
 class Sorting(Resource):
     def get(self):
@@ -23,128 +37,7 @@ class Sorting(Resource):
     def post(self):
         return qsort(request.get_json(force=True))
 
-
-class StringCompression(Resource):
-    def get(self, mode):
-        return "This is the String Compression Problem"
-
-    def post(self, mode):
-        json_data = request.get_json(force=True)
-        string_value = json_data['data']
-
-        if(mode == "LZW"):
-            answer = compress(string_value)
-        elif(mode == "RLE"):
-            answer = compress1(string_value)
-        else:
-            answer = compressWDE(string_value)
-
-        return answer
-
-class JewelleryHeist(Resource):
-    def get(self):
-        return "This is the Jewellery Heist Problem"
-
-    def post(self):
-        json_data = request.get_json(force=True)
-        weight = json_data["maxWeight"]
-        vault = json_data["vault"]
-
-        answer = fakeknapsack(vault, weight)
-        answer_json = {"heist": answer}
-        return jsonify(answer_json)  
-
-class HorseRacing(Resource):
-    def get(self):
-        return "This is the Horse Racing Problem"
-
-    def post(self):
-        json_data = request.get_json(force=True)
-        data = json_data['data']
-
-        answer = horsenipples(data)
-
-        return jsonify(answer)
-
-class ReleaseScheduler(Resource):
-    def get(self):
-        return "This is the Release Scheduler Problem"
-
-    def post(self):
-        raw_list = request.get_data()
-        raw_list_string = raw_list.decode("utf-8")
-        list_string = ast.literal_eval(raw_list_string)
-        answer = scheduler(list_string)
-        return answer
-
-class TrainPlanner(Resource):
-    def get(self):
-        return "This is the Train Planner Problem"
-
-    def post(self):
-        json_data = request.get_json(force=True)
-        destination = json_data['destination']
-        stations = json_data['stations']
-        answer = busybody(destination, stations)
-
-        return answer
-
-class CalculateEmptyArea(Resource):
-    def get(self):
-        return "This is the Empty Area Problem"
-
-    def post(self):
-        json_data = request.get_json(force=True)
-        answer = 0
-        
-        con_X = json_data["container"]["coordinate"]["X"]
-        con_Y = json_data["container"]["coordinate"]["Y"]
-        con = [con_X, con_Y]
-
-        condim_width = json_data["container"]["width"]
-        condim_height = json_data["container"]["height"]
-        condim = [condim_width, condim_height]
-
-        if "rectangle" in json_data:
-            sha_X = json_data["rectangle"]["coordinate"]["X"]
-            sha_Y = json_data["rectangle"]["coordinate"]["Y"]
-            sha = [sha_X, sha_Y]
-
-            shadim_width = json_data["rectangle"]["width"]
-            shadim_height = json_data["rectangle"]["height"]
-            shadim = [shadim_width, shadim_height]
-
-            answer = rectangle(con, condim, sha, shadim)
-
-        if "square" in json_data:
-            sha_X = json_data["square"]["coordinate"]["X"]
-            sha_Y = json_data["square"]["coordinate"]["Y"]
-            sha = [sha_X, sha_Y]
-
-            shadim_width = json_data["square"]["width"]
-            shadim = [shadim_width, shadim_width]
-            
-            answer = rectangle(con, condim, sha, shadim)
-
-        if "circle" in json_data:
-            sha_X = json_data["circle"]["center"]["X"]
-            sha_Y = json_data["circle"]["center"]["Y"]
-            sha = [sha_X, sha_Y]
-
-            radius = json_data["circle"]["radius"]
-
-            answer = circle(con, condim, sha, radius)
-
-        return answer
-
-
 api.add_resource(Sorting, '/sort')
-api.add_resource(StringCompression, '/stringcompression/<mode>')
-api.add_resource(TrainPlanner, '/trainPlanner')
-api.add_resource(CalculateEmptyArea, '/calculateemptyarea')
-api.add_resource(JewelleryHeist, '/heist')
-api.add_resource(ReleaseScheduler, '/releaseSchedule')
-api.add_resource(HorseRacing, '/horse-racing')
 
 if __name__ == '__main__':
     app.run(debug=True)
