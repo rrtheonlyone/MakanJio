b696dfbfde1721dec1572e2d5b308da2d793c4ea
prokarius <rollingwithoutslipping@yahoo.com>
2018-01-28 02:18:21 +0800

    Added the API for home page

--- server/app.py
+++ server/app.py
@@ -19,6 +19,7 @@ api = Api(app)
 class Person(db.Model):
     personId = db.Column(db.Integer, primary_key=True)
     personName = db.Column(db.String(120), nullable=False)
+    personDesc = db.Column(db.String(2000), nullable=False)
 
     # receivedFeedbacks = db.relationship('Feedback', secondary=feedbacks, lazy='subquery',
     #     backref=db.backref('persons', lazy=True))
@@ -40,9 +41,12 @@ class Food(db.Model):
     cookId = db.Column(db.Integer, db.ForeignKey('person.personId'))
     locationLong = db.Column(db.Float)
     locationLat = db.Column(db.Float)
-    price = db.Column(db.Integer)
+    price = db.Column(db.Integer) #In cents
     description = db.Column(db.String(2000))
-    
+    datetime = db.Column(db.Integer) #Datetime in yymmddhhmm
+    cuisine = db.Column(db.String(150))
+    quota = db.Column(db.Integer)
+
     attendees = db.relationship('Person', secondary=attendees, lazy='subquery',
         backref=db.backref('foods', lazy=True))
     feedbacks = db.relationship('Feedback', backref='foodEvent', lazy=True)
@@ -54,31 +58,56 @@ class Food(db.Model):
 class Feedback(db.Model):
     feedbackId = db.Column(db.Integer, primary_key=True)
     feedbackAuthorId = db.Column(db.Integer, db.ForeignKey('person.personId'))
+    rating = db.Column(db.Integer)
     foodId = db.Column(db.Integer, db.ForeignKey('food.foodId'))
     message = db.Column(db.String(2000))
 
 #ducttape
-def getDetails(id):
-    o = [] #debug
+#returns a Dict
+def getFeedback(id):
+    o = {} #debug
     foodEvent = Food.query.filter_by(foodId=id).first()
-    print("Description: " + foodEvent.description)
+    o["description"] = foodEvent.description
     cook = foodEvent.cook
-    print("Cook name: " + cook.personName)
-    print("Feedback about the event")
+    o["cook"] = cook.personName
+    feedbacklist = []
     for feedback in foodEvent.feedbacks:
-        o += "From " + feedback.feedbackAuthor.personName + ": " + feedback.message
+        feedbacklist.append({feedback.feedbackAuthor.personName: feedback.message})
+    o['feedback'] = feedbacklist
     return o
 
-
 class getAll(Resource):
+    #Returns a list of dicts
     def get(self):
-        return "LOL"
+        o = []
+        for event in Food.query.all():
+            collect = {}
+            collect['foodId'] = event.foodId
+            collect['cookId'] = event.cookId
+            collect['long'] = event.locationLong
+            collect['lat'] = event.locationLat
+            collect['price'] = event.price
+            collect['description'] = event.description
+            collect['datetime'] = event.datetime
+            collect['cuisine'] = event.cuisine
+            collect['quota'] = event.quota
+            guestlist = []
+            for attendee in event.attendees:
+                guestlist.append(attendee.personName)
+            collect['attendees'] = guestlist
+            feedbacklist = []
+            for feedback in event.feedbacks:
+                feedbacklist.append({feedback.feedbackAuthor.personName: feedback.message})
+            collect['feedback'] = feedbacklist
+            o.append(collect)
+        return o
+
     def post(self):
         pass
 
 class getFood(Resource):
     def get(self, foodid):
-        getDetails(1)
+        return getFeedback(foodid)
 
 api.add_resource(getAll, '/cook/all')
 api.add_resource(getFood, '/cook/<int:foodid>')
